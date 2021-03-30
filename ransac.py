4355ae98d09c67171881186eda60d9985f179787
239473 <239473@edu.p.lodz.pl>
2021-03-30 20:43:03 +0200

    modif. ransac with data plot

--- ransac.py
+++ ransac.py
@@ -15,4 +15,13 @@ cloud=numpy.array(read_csv("cloud_r.xyz"))
 model_robotus, inliers=ransac(cloud,LineModelND,min_samples=2,residual_threshold=1,max_trials=1000)
 outliers=inliers==False
 
+import matplotlib.pyplot as plt
+
+fig=plt.figure()
+ax=fig.add_subplot(111, projection='3d')
+ax.scatter(cloud[inliers][:,0],cloud[inliers][:,1],cloud[inliers][:,2],c='g',marker='o',label='inliers')
+# ax.scatter(cloud[outliers][:,0],cloud[outliers][:,1],cloud[outliers][:,2],c='r',marker='o',label='outliers')
+ax.legend(loc='lower left')
+plt.show()
+
 print(inliers)
